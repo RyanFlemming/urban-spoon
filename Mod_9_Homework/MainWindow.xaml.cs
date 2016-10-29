187dee0251d6a6b48a5f9ea3cf66d6b277b56a23
Ryan <rmf919@live.com>
2016-10-29 17:44:59 +1100

    initial commit

---
+++ Mod_9_Homework/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mod_9_Homework
+{
+	public partial class MainWindow : Window
+	{
+		int counter = 0; // For searching students
+		List<Student> studentCollection = new List<Student>();
+
+		Student searchResult = new Student();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void btnCreateStudent_Click(object sender, RoutedEventArgs e)
+		{
+			counter++;
+			// Creating our student and assigning values from text boxes
+			Student student = new Student();
+			
+			student.ID = counter;
+			student.FirstName = txtFirstName.Text;
+			student.LastName = txtLastName.Text;
+			student.City = txtCity.Text;
+
+			// Adding student to our list collection
+			studentCollection.Add(student);
+
+			Clear();
+		}
+
+		private void Clear()
+		{
+			txtFirstName.Clear();
+			txtLastName.Clear();
+			txtCity.Clear();
+		}
+
+		private void btnPrevious_Click(object sender, RoutedEventArgs e)
+		{
+			counter -= 1;
+			if (counter < 0)
+			{
+				counter = 0;
+			}
+			searchResult = studentCollection[counter] as Student;
+			DisplayResult(searchResult);
+		}
+
+		private void btnNext_Click(object sender, RoutedEventArgs e)
+		{
+			counter += 1;
+			if (counter == studentCollection.Count)
+			{
+				counter = 0;
+				MessageBox.Show("Last result reached. Wrapping around.");
+			}
+			Student searchResult = studentCollection[counter] as Student;
+			DisplayResult(searchResult);
+		}
+
+		private void DisplayResult(Student student)
+		{
+			txtFirstName.Text = student.FirstName;
+			txtLastName.Text = student.LastName;
+			txtCity.Text = student.City;
+		}
+	}
+}
---
+++ Mod_9_Homework/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_9_Homework
+{
+	public class Student
+	{
+		private int id;
+		private string firstName;
+		private string lastName;
+		private string city;
+
+		public string City
+		{
+			get { return city; }
+			set { city = value; }
+		}
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+		public int ID
+		{
+			get { return id; }
+			set { id = value; }
+		}
+	}
+}
